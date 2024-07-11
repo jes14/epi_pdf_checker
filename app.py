7982ebef12069ddad8c0fbef7c06aac2a5ff8a25
jes14 <yj25058@gmail.com>
2024-07-11 09:34:40 +0200

    update post router

--- app.py
+++ app.py
@@ -101,6 +101,6 @@ def search():
 
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
+    # if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    #     os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True)
