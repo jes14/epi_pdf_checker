4837546ae72c1576f535261e1129fea3919b87f5
jes14 <yj25058@gmail.com>
2024-07-12 13:10:16 +0200

    edit keyword extract func

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from utils.file_handling import allowed_file
 from utils.pdf_processing import extract_text_from_pdf, correct_tokenization
 from utils.keyword_search import create_keyword_list, search_keywords
-from utils.nlp_extraction import extract_nlp_keywords
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -43,9 +42,8 @@ def search():
     text = request.form['text']
     keywords = create_keyword_list()
     results = search_keywords(text, keywords)
-    nlp_keywords = extract_nlp_keywords(text)
     
-    return render_template('results.html', results=results, nlp_keywords=nlp_keywords)
+    return render_template('results.html', results=results)
 
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
